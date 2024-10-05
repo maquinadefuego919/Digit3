85afe93090a9daa93cce5d9beb1bc32b221a06c5
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-05 12:34:09 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_Digit3 (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
@@ -19,5 +19,19 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+  assign [0] ui_in  =Vip;
+        assign [1] ui_in  =Vin;
+    assign [0] ui_out  =Out
+wire INn, INp, CMP, EN, not_EN, Op, On; //internals nets
+not IV1 (INn, Vip);
+not IV2 (INn, CMP);
+not IV3 (INp, Vin);
+not IV4 (INp, CMP);
+not IV5 (Op, INn);
+not IV6 (On, INp);
+not IV7 (not_EN, EN);
+xor XOR1 (EN, Op, On);
+bufif1 BT1 (Out, EN, Op);
+notif1 IT1 (CMP, not_EN, Op);
 
 endmodule
