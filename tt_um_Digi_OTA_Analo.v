839c208b426bc8ed5757d960518eeb691c24f0c1
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-10 16:50:44 -0700

    Update and rename src/project.v to  tt_um_Digi_OTA_Analo.v

--- tt_um_Digi_OTA_Analo.v
+++ tt_um_Digi_OTA_Analo.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_Digit3 (
+module tt_um_Digi_OTA_Analo (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
@@ -19,19 +19,34 @@ module tt_um_Digit3 (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-  assign [0] ui_in  =Vip;
-        assign [1] ui_in  =Vin;
-    assign [0] ui_out  =Out
-wire INn, INp, CMP, EN, not_EN, Op, On; //internals nets
-not IV1 (INn, Vip);
-not IV2 (INn, CMP);
-not IV3 (INp, Vin);
-not IV4 (INp, CMP);
-not IV5 (Op, INn);
-not IV6 (On, INp);
-not IV7 (not_EN, EN);
-xor XOR1 (EN, Op, On);
-bufif1 BT1 (Out, EN, Op);
-notif1 IT1 (CMP, not_EN, Op);
+    wire Vip, Vin, Out;
+
+    assign Vip = ui_in[0];
+    assign Vin = ui_in[1];
+    
+    assign uo_out[0]  = Out;  
+    assign uo_out[7:1] = 7'b0000000; 
+
+    wire INn, INp, INn_CMP, INp_CMP, CMP, EN, not_EN, Op, On, INn_AND, INp_AND; //internals nets 
+    not IV1(INn, Vip);    
+    not INV2(INn_CMP,CMP);
+    not IV3(INp, Vin);
+    not INV4(INp_CMP,CMP);
+
+    nor NOR1(Op, INn, INn_CMP);
+    nor NOR2(On, INp, INp_CMP);
+    
+    //not IV5(Op, INn_AND);
+    //not IV6(On, INp_AND);
+    
+    xor XOR1(EN, Op, On);
+    
+    not IV7(not_EN, EN);
+    notif1 IT1(CMP, not_EN, Op);  
+    
+    bufif1 BT1(Out, EN, Op);   
+
+    
+  
 
 endmodule
